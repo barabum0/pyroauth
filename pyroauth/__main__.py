ff640431864a359e129da0eb104e74461888a504
roman mac <barabum@duck.com>
2023-12-02 14:46:12 +0300

    :sparkles: feat: add session output path and post-auth user feedback

--- pyroauth/__main__.py
+++ pyroauth/__main__.py
@@ -3,10 +3,14 @@ import click
 from pyrogram import Client
 
 
-async def auth(api_id: int, api_hash: str):
-    client = Client("client", api_id=api_id, api_hash=api_hash)
+async def auth(api_id: int, api_hash: str, output: str):
+    client = Client(output, api_id=api_id, api_hash=api_hash, in_memory=False)
     async with client:
         await client.authorize()
+        me = await client.get_me()
+        click.echo(f"""
+        Successfully authorized as {me.first_name} {me.last_name if me.last_name else ""}
+        """)
 
 
 @click.command()
@@ -14,8 +18,11 @@ async def auth(api_id: int, api_hash: str):
               help="Telegram app API ID. Can be obtained from https://my.telegram.org/apps")
 @click.option("--api_hash", "--hash", "-ah", type=click.STRING, required=True,
               help="Telegram app API hash. Can be obtained from https://my.telegram.org/apps")
-def main(api_id: int, api_hash: str):
-    asyncio.run(auth(api_id, api_hash))
+@click.option("--output", "-o", type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True),
+              help=".session file output path", default="user.session")
+def main(api_id: int, api_hash: str, output: str):
+    output = output.removesuffix(".session")
+    asyncio.run(auth(api_id, api_hash, output))
 
 
 if __name__ == '__main__':
