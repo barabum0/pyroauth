1f23aba7193b599c73f50f30859d3ea821a4e098
roman mac <barabum@duck.com>
2023-12-02 14:35:29 +0300

    :sparkles: feat: add CLI support with options for API key and hash in pyroauth

--- pyroauth/__main__.py
+++ pyroauth/__main__.py
@@ -1,16 +1,21 @@
 import asyncio
-
+import click
 from pyrogram import Client
 
 
-async def auth():
+async def auth(api_key: str, api_hash: str):
     client = Client("client")
     async with client:
         await client.authorize()
 
 
-def main():
-    asyncio.run(auth())
+@click.command()
+@click.option("--api_key", "--key", "-ak", type=click.STRING, required=True,
+              help="Telegram app API key. Can be obtained from https://my.telegram.org/apps")
+@click.option("--api_hash", "--hash", "-ah", type=click.STRING, required=True,
+              help="Telegram app API hash. Can be obtained from https://my.telegram.org/apps")
+def main(api_key: str, api_hash: str):
+    asyncio.run(auth(api_key, api_hash))
 
 
 if __name__ == '__main__':
