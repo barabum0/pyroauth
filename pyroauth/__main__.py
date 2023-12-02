2bbe3fa6a76600ac607615d1aec95fe5fe2fb1dc
roman mac <barabum@duck.com>
2023-12-02 14:39:28 +0300

    :sparkles: feat: add required `--api_hash` option to CLI

--- pyroauth/__main__.py
+++ pyroauth/__main__.py
@@ -3,18 +3,18 @@ import click
 from pyrogram import Client
 
 
-async def auth(api_id: str, api_hash: str):
+async def auth(api_id: int, api_hash: str):
     client = Client("client", api_id=api_id, api_hash=api_hash)
     async with client:
         await client.authorize()
 
 
 @click.command()
-@click.option("--api_key", "--key", "-ak", type=click.STRING, required=True,
-              help="Telegram app API key. Can be obtained from https://my.telegram.org/apps")
-@click.option("--api_id", "--id", "-ai", type=click.STRING, required=True,
+@click.option("--api_id", "--id", "-ai", type=click.INT, required=True,
               help="Telegram app API ID. Can be obtained from https://my.telegram.org/apps")
-def main(api_id: str, api_hash: str):
+@click.option("--api_hash", "--hash", "-ah", type=click.STRING, required=True,
+              help="Telegram app API hash. Can be obtained from https://my.telegram.org/apps")
+def main(api_id: int, api_hash: str):
     asyncio.run(auth(api_id, api_hash))
 
 
